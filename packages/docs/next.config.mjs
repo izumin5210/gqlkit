7beb07b3cc22e1670da0d5c236da36c2f7df05c3
izumin5210 <m@izum.in>
2026-01-11 13:19:00 +0900

    use catppuccin for syntax highlight

--- packages/docs/next.config.mjs
+++ packages/docs/next.config.mjs
@@ -2,6 +2,14 @@ import nextra from "nextra";
 
 const withNextra = nextra({
   contentDirBasePath: "/",
+  mdxOptions: {
+    rehypePrettyCodeOptions: {
+      theme: {
+        dark: "catppuccin-mocha",
+        light: "catppuccin-latte",
+      },
+    },
+  },
 });
 
 export default withNextra({
