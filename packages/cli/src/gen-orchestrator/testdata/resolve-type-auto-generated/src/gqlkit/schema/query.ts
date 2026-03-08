a7ac60badcf845c97823f61663144f70cd2148df
izumin5210 <m@izum.in>
2026-03-08 13:52:21 +0900

    `pnpm -w check`

--- packages/cli/src/gen-orchestrator/testdata/resolve-type-auto-generated/src/gqlkit/schema/query.ts
+++ packages/cli/src/gen-orchestrator/testdata/resolve-type-auto-generated/src/gqlkit/schema/query.ts
@@ -1,5 +1,5 @@
-import type { Container } from "./types.js";
 import { defineQuery } from "../gqlkit.js";
+import type { Container } from "./types.js";
 
 export const container = defineQuery<{ id: string }, Container>(() => ({
   id: "1",
